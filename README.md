ba92740d0ecca64dcd6a80ffe75a5b719f2e8ba7
john <john@bolodeMac-mini.local>
2017-09-30 17:15:36 +0800

    `npm start` to run project in develop mode

--- README.md
+++ README.md
@@ -1,8 +1,13 @@
 # DemoBlockly
-React方式集成blockly
+### React方式集成blockly
 
-项目很简单，采用了es6语法和jsx语法，为方便使用这些特性，使用webpack打包项目</br>
-主要的代码是 blocklyDiv.js 中如何 Blockly.inject 。这个代码是从另一个[repo](https://github.com/patientslikeme/react-blockly-component)中看来的</br>
+项目很简单，采用了es6语法和jsx语法，为方便使用这些特性，使用webpack打包项目<br/>
+主要的代码是`blocklyDiv.js`中如何 Blockly.inject 。这个代码是从另一个[repo](https://github.com/patientslikeme/react-blockly-component)中看来的<br/>
 我觉得没必要啊，本来很简单的一个配置，我不喜欢整这么复杂。注意正确设置div的大小
 
-mac下运行方法：npm i && npm run build && open index.html
+### 运行方法
+开发模式运行项目
+1. npm install
+2. npm start
+
+由于项目我已经生成了，所以直接打开`dist/index.html`也可以查看效果
---
+++ webpack.dev.js
@@ -0,0 +1,38 @@
+var path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  devtool: "inline-source-map",
+  devServer: {
+    contentBase: './dist',
+    lazy: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        include: /src/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: ['es2015', 'stage-2', 'react'],
+              plugins: ['transform-runtime']
+            }
+          }
+        ]
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      }
+    ]
+  },
+};
